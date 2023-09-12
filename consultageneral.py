a98b0f2ead0999b8717006dec543947e9832f3c9
Yilmar Rivera <yegereve@gmail.com>
2023-09-12 01:12:31 -0500

    se agrega usuario y contraseña

---
+++ consultageneral.py
@@ -0,0 +1,4 @@
+variable_texto = "Agenda de Citas Medicas"
+nueva_variable_texto_reemplazo = variable_texto.replace("Agenda de Citas Medicas"," Consulta General")
+print(variable_texto)
+print(nueva_variable_texto_reemplazo)
--- pediatria.py
+++ pediatria.py
@@ -1,14 +1,38 @@
-#variable_texto = "Agenda " # String - Texto
-#print(variable_texto.upper())
-#print(variable_texto.lower())
-#print(variable_texto.upper().find(str.lower("GE")))
-#nueva_variable_texto_reemplazo = variable_texto.replace("Agenda","Citas Medicas Especialistas")
-#print (variable_texto.replace("Agenda","Citas Medicas "))
-#print(variable_texto)
-#print(nueva_variable_texto_reemplazo)
-#print("Agenda" in nueva_variable_texto_reemplazo)
-#print("Agenda" in variable_texto)
-dato_ingresado = input("Digite su usuario:")
-print (type(dato_ingresado))
-#valor = 18 int(dato_ingresado)
-#print ("valor * 3 " )
+variable_texto = "Agenda de Citas Medicas"
+nueva_variable_texto_reemplazo = variable_texto.replace("Agenda de Citas Medicas"," Pediatria")
+print(variable_texto)
+print(nueva_variable_texto_reemplazo)
+
+dato_ingresado = input("Ingrese su Usuario:")
+variable_usuario = str(dato_ingresado)
+if(variable_usuario <  "ygrivera"):
+   print("Usuario incorrecto")
+elif(variable_usuario == "ygrivera"):
+   print("Usuario Correcto")
+elif(variable_usuario > "ygrivera"and variable_usuario < "ygrivera"):
+   print("usuario correcto")    
+#else:
+    #print("Bienvenido")
+def comprobar_Contraseña(Password):
+    largo = False
+    mayus = False
+    numer = False
+    if len (Password) > 8:
+        largo= True
+    for i in range(len(Password)):
+        if Password[i].isupper():
+            mayus = True
+        if Password[i].isnumeric():
+            numer= True
+
+    if largo and mayus and numer:
+        return True
+    else:
+        return False
+    
+Password = input ("Ingrese Una Contraseña: ")
+verificacion = comprobar_Contraseña (Password)
+if verificacion:
+    print ("la contraseña es segura")
+else:
+    print("La Contraseña no es Segura")
